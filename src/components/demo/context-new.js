724529c55528ee552259cec13f904f02ab8b7d72
dingminghui <dingminghui123@163.com>
2021-03-15 22:38:44 +0800

    content and render props

--- src/components/demo/context-new.js
+++ src/components/demo/context-new.js
@@ -1,16 +1,84 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export default class ContextNew extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
+const ctx = React.createContext()
 
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+// 在函数组件中，需要使用Consumer来获取上下文数据
+function ChildA(props) {
+    return <div>
+        <h1>ChildA</h1>
+        <h2>
+            <ctx.Consumer>
+                {value => <>{value.a}，{value.b}</>}
+            </ctx.Consumer>
+        </h2>
+        <ChildB />
+    </div>
+}
+
+// 在类组件中，直接使用this.context获取上下文数据
+class ChildB extends React.Component {
+  static contextType = ctx;
+
+  // 如果，上下文提供者（Context.Provider）中的value属性发生变化(Object.is比较)，
+  // 会导致该上下文提供的所有后代元素全部重新渲染，无论该子元素是否有优化（无论shouldComponentUpdate函数返回什么结果）
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('运行了优化')
+    return false
+  }
+
+  render() {
+    console.log('childB render')
+    return (
+      <h1>
+        {/* a:{this.props.a}，b:{this.props.b} */}
+        a:{this.context.a}，b:{this.context.b}
+      </h1>
+    )
+  }
+}
+
+export default class NewContext extends Component {
+  state = {
+    a: 0,
+    b: 'abc',
+    changeA: (newA) => {
+      this.setState({
+        a: newA,
+      })
+    },
+  }
+
+  render() {
+    return (
+      //   <div>
+      //     <ChildB {...this.state} />
+      //     <button
+      //       onClick={() => {
+      //         this.setState({
+      //           a: this.state.a + 1,
+      //         })
+      //       }}
+      //     >
+      //       父组件的按钮，a加1
+      //     </button>
+      //   </div>
+
+      <ctx.Provider value={this.state}>
+        <div>
+          <ChildA />
+
+          <button
+            onClick={() => {
+              this.setState({
+                a: this.state.a + 1,
+              })
+            }}
+          >
+            父组件的按钮，a加1
+          </button>
+        </div>
+      </ctx.Provider>
+    )
+  }
 }
--- src/index.js
+++ src/index.js
@@ -5,18 +5,46 @@ import ClassCom from './components/demo/class'
 import CheckBox from './components/form/checkbox'
 import RefDemo from './components/demo/ref'
 import ContextOld from './components/demo/context-old'
+import ContextNew from './components/demo/context-new'
+import RenderProps from './components/demo/renderProps'
 
 const obj = { name: 'zyl', age: 30, time: 30 }
 const funcObj = { name: 'dmh', age: 24, content: <div>content</div> }
+const renderPoint = (mouse) => (
+  <>
+    横坐标：{mouse.x}，纵坐标：{mouse.y}
+  </>
+)
+const renderDiv = (mouse) => (
+  <>
+    <div
+      style={{
+        width: 100,
+        height: 100,
+        background: '#008c8c',
+        position: 'absolute',
+        left: mouse.x - 50,
+        top: mouse.y - 50,
+      }}
+    ></div>
+  </>
+)
 
 ReactDOM.render(
   <h1>
-    Hello, world!
-    <FunctionalCom attr={funcObj} />
-    <ClassCom attr={obj} />
-    <CheckBox />
-    <RefDemo />
-    <ContextOld />
+    <div>
+      Hello, world!
+      <FunctionalCom attr={funcObj} />
+      <ClassCom attr={obj} />
+      <CheckBox />
+      <RefDemo />
+      <ContextOld />
+      <ContextNew />
+    </div>
+    <div>
+      <RenderProps render={renderPoint} />
+      <RenderProps render={renderDiv} />
+    </div>
   </h1>,
   document.getElementById('root')
 )
